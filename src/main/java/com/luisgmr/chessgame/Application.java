737b179f9c09b7cd16f8da51df48454fea9acc30
Luis Gustavo Miranda <luisgmr.privado@hotmail.com>
2023-06-09 22:54:13 -0300

    Ícone personalizado

--- src/main/java/com/luisgmr/chessgame/Application.java
+++ src/main/java/com/luisgmr/chessgame/Application.java
@@ -3,6 +3,7 @@ package com.luisgmr.chessgame;
 import com.luisgmr.chessgame.controller.BoardController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,10 @@ public class Application extends javafx.application.Application {
         Scene scene = new Scene(fxmlLoader.load(), 1280, 960);
         stage.setTitle("Xadrez");
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setMaximized(false);
+        Image icon = new Image(getClass().getResourceAsStream("/com/luisgmr/chessgame/images/icone.png"));
+        stage.getIcons().add(icon);
         stage.show();
     }
 
